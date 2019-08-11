9410cbd578c1b0f712b20dcbb10f6710fd222362
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2019-08-11 23:49:28 +0300

    add Portfolio component

--- src/containers/Portfolio/Portfolio.js
+++ src/containers/Portfolio/Portfolio.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Aux from '../../hoc/_Aux'
 
 const portfolio = props => {
     return (
-        <div>
-            PORTFOLIO
-        </div>
+        <Aux>
+            
+        </Aux>
     )
 };
 
