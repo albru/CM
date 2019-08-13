db8ce0f28d91f2821f9b3d69c97fb9052df3787a
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2019-08-13 22:52:55 +0300

    add data objects to separate files

---
+++ src/store/data/servicesData.js
@@ -0,0 +1,27 @@
+import outdoorAdvImg from '../../assets/img/services/outdoorAdvImg.png';
+import interiorAdvImg from '../../assets/img/services/interiorAdvImg.png';
+import advServicesImg from '../../assets/img/services/advServicesImg.png';
+
+export const servicesData = [
+    {
+        title: 'Наружная реклама',
+        href: '/11',
+        imgSrc: outdoorAdvImg,
+        imgAlt: 'outdoorAdvImg',
+        desc: 'Фасадные вывески • Объёмные буквы • Согласование вывесок • Крышные установки • Козырёк над крыльцом • Летние кафе • Рекламные стелы • Бегущие строки • Входная группа • Панель-кронштейны • Световые короба • Изготовление металлоконструкций'
+    },
+    {
+        title: 'Интерьерная реклама',
+        href: '/22',
+        imgSrc: interiorAdvImg,
+        imgAlt: 'interiorAdvImg',
+        desc: 'Зоны ресепшн • Кассовые зоны • Неоновые вывески • Световые панели • Выставочные стенды • Изготовление табличек • Информационные стенды • Изделия из дерева'
+    },
+    {
+        title: 'Рекламные услуги',
+        href: '/33',
+        imgSrc: advServicesImg,
+        imgAlt: 'advServicesImg',
+        desc: 'Дизайн рекламы • Нейминг • Широкоформатная печать • Плоттерная резка • Полиграфия • Ремонт вывесок • Промышленный альпинизм'
+    }
+]
