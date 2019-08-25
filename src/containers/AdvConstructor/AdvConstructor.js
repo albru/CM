eb4c3c358c641966f1b4c8a8b2bab84ecbd67c87
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2019-08-25 21:32:35 +0300

    test version for deploy

--- src/containers/AdvConstructor/AdvConstructor.js
+++ src/containers/AdvConstructor/AdvConstructor.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import classes from './AdvConstructor.css';
 import Button from '../../components/UI/Button/Button';
+import BreadCrumbs from '../../components/Navigation/BreadCrumbs/BreadCrumbs';
 
 const AdvConstructor = props => {
     const buttonRedirectHandler = () => {
         props.history.push('/');
     }
+
+    const crumbs = [
+        {
+            crumbName:'Главная',
+            crumbHref: '/',
+            linkType: 'prev'},
+        {
+            crumbName:'Конструктор вывесок',
+            crumbHref: '/constructor',
+            linkType: 'current'
+        }
+    ];
+
     return (
-        <div className={classes.AdvConstructor}>
+        <section className={classes.AdvConstructor}>
+            <BreadCrumbs crumbs={crumbs} />
             <h2> Конструктор вывесок временно недоступен.  </h2>
             <Button 
                 btnType='MainButton'
                 clicked={buttonRedirectHandler}> На главную </Button> 
-        </div>
+        </section>
         
     )
 };
