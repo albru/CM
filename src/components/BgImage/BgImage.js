df455f6ed1df8cdffd1b268ac3c61e9a5699ba59
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2019-08-31 17:32:18 +0300

    add function which check user browser type for loading apropriate img (webp or jpg/png)

--- src/components/BgImage/BgImage.js
+++ src/components/BgImage/BgImage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import bgImg from '../../assets/img/main-bg.webp';
+import bgImgPng from '../../assets/img/main-bg.png';
 import classes from './BgImage.css';
+import { checkUserBrowserForRightImages } from '../../shared/utility';
 
 const bgImage = props => {
+    let changedImage = checkUserBrowserForRightImages(null) ? bgImgPng : bgImg;
     return (
         <div className={classes.BgImage}>
-            <img src={bgImg} alt='background'></img>
+            <img src={changedImage} alt='background'></img>
         </div>
     )
 }
--- src/components/Contacts/Map/Map.js
+++ src/components/Contacts/Map/Map.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import mapImg from '../../../assets/img/map/map.webp';
+import mapImgPng from '../../../assets/img/map/map.png';
 import classes from './Map.css';
+import { checkUserBrowserForRightImages } from '../../../shared/utility';
 
 const map = () => {
+    let changedImage = checkUserBrowserForRightImages(null) ? mapImgPng : mapImg;
     return (
         <div className={classes.Map}>
-            <img src={mapImg} alt="map"></img>
+            <img src={changedImage} alt="map"></img>
         </div>
         
     )
