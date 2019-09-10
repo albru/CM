1f6813b95a22732a10abdcf3b63e2540dc3fac9e
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2019-09-10 13:05:12 +0300

    testing

--- src/components/UI/Backdrop/backdrop.test.js
+++ src/components/UI/Backdrop/backdrop.test.js
@@ -3,14 +3,14 @@ import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import NavigationItem from './NavigationItem';
+import Backdrop from './Backdrop';
 
 configure({ adapter: new Adapter() })
 
-describe('NavigationItem', () => {
+describe('Modal', () => {
     it('should render correctly', () => {
         const output = shallow(
-            <NavigationItem name="mockName" route="mockRoute" />
+            <Backdrop />
         );
         expect(shallowToJson(output)).toMatchSnapshot();
     });
