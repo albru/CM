22b4815f6bb2bdc6f064fab793859da72a8f602a
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2020-06-12 17:55:21 +0200

    m

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,97 +1,105 @@
-import * as actionTypes from './actionTypes';
+import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
-    return {
-        type: actionTypes.AUTH_START
-    };
+  return {
+    type: actionTypes.AUTH_START,
+  };
 };
 
 export const authSuccess = (idToken, userId) => {
-    return {
-        type: actionTypes.AUTH_SUCCESS,
-        idToken: idToken,
-        userId: userId
-    };
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    idToken: idToken,
+    userId: userId,
+  };
 };
 
 export const authError = (error) => {
-    return {
-        type: actionTypes.AUTH_ERROR,
-        error: error
-    };
+  return {
+    type: actionTypes.AUTH_ERROR,
+    error: error,
+  };
 };
 
 export const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('expirationDate');
-    localStorage.removeItem('userId');
-    return {
-        type: actionTypes.AUTH_LOGOUT
-    };
+  localStorage.removeItem("token");
+  localStorage.removeItem("expirationDate");
+  localStorage.removeItem("userId");
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+  };
 };
 
 export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout())
-        }, expirationTime * 1000)
-    };
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
 };
 
 export const tryAuth = () => {
-    return {
-        type: actionTypes.AUTH_TRY_AUTOLOGIN
-    };
-}
+  return {
+    type: actionTypes.AUTH_TRY_AUTOLOGIN,
+  };
+};
 
 export const auth = (email, password, isSignUp) => {
-    return dispatch => {
-        dispatch(authStart());
-        let url = isSignUp ? 
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCdNuG1zpK5PHbd45tAlMk8kBoP_p7B3hM'
-        :
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCdNuG1zpK5PHbd45tAlMk8kBoP_p7B3hM'
-        
-        fetch(url, {
-                method: 'POST',
-                body: JSON.stringify({
-                    email: email,
-                    password: password,
-                    returnSecureToken: true
-                }),
-                headers: {'Content-Type': 'application/json'}
-            }).then(response => {
-                if(!response.ok) { throw response }
-                return response.json();
-            }).then(responseData => {
-                const expirationDate = new Date(new Date().getTime() + responseData.expiresIn * 1000)
-                localStorage.setItem('token', responseData.idToken)
-                localStorage.setItem('expirationDate', expirationDate)
-                localStorage.setItem('userId', responseData.localId)
-                dispatch(authSuccess(responseData.idToken, responseData.localId));
-                dispatch(checkAuthTimeout(responseData.expiresIn));
-            }).catch(error => {
-                console.log(error.toString())
-                dispatch(authError(error.toString()))
-            })
-    }
+  return (dispatch) => {
+    dispatch(authStart());
+    let url = isSignUp ? action1 : action2;
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw response;
+        }
+        return response.json();
+      })
+      .then((responseData) => {
+        const expirationDate = new Date(
+          new Date().getTime() + responseData.expiresIn * 1000
+        );
+        localStorage.setItem("token", responseData.idToken);
+        localStorage.setItem("expirationDate", expirationDate);
+        localStorage.setItem("userId", responseData.localId);
+        dispatch(authSuccess(responseData.idToken, responseData.localId));
+        dispatch(checkAuthTimeout(responseData.expiresIn));
+      })
+      .catch((error) => {
+        console.log(error.toString());
+        dispatch(authError(error.toString()));
+      });
+  };
 };
 
 export const authCheckState = () => {
-    return dispatch => {
-        dispatch(tryAuth())
-        const token = localStorage.getItem('token');
-        if (!token) {
-            dispatch(logout())
-        } else {
-            const expirationDate = new Date(localStorage.getItem('expirationDate'));
-            if (expirationDate <= new Date()) {
-                dispatch(logout())
-            } else {
-                const userId = localStorage.getItem('userId')
-                dispatch(authSuccess(token, userId))
-                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000 ));
-            }
-        }
-    };
+  return (dispatch) => {
+    dispatch(tryAuth());
+    const token = localStorage.getItem("token");
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem("expirationDate"));
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
+        const userId = localStorage.getItem("userId");
+        dispatch(authSuccess(token, userId));
+        dispatch(
+          checkAuthTimeout(
+            (expirationDate.getTime() - new Date().getTime()) / 1000
+          )
+        );
+      }
+    }
+  };
 };
